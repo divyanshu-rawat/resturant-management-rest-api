e45b1c14c6ccb9260eddbb8e41a02e7cce830626
divyanshu <divyanshu.r46956@gmail.com>
2016-12-19 16:10:17 +0530

    Implemented GET,PUT,DELETE for a specific dish !

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -69,23 +69,57 @@ leaderRouter.route('/')
   
   leaderRouter.route('/:leaderId')
   
-  .all(function(req,res,next) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    next();
-  })
+  // .all(function(req,res,next) {
+  //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //   next();
+  // })
 
   .get(function(req,res,next){
-    res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+    // res.end('Will send details of the leader: ' + req.params.leaderId +' to you!');
+
+
+       leaderships.findById(req.params.leaderId,function(err,leadership){
+
+          if(err) throw err;
+          res.json(leadership);
+
+        });
+
   })
 
   .put(function(req, res, next){
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name +
-      ' with details: ' + req.body.description);
+
+    // res.write('Updating the leader: ' + req.params.leaderId + '\n');
+    // res.end('Will update the leader: ' + req.body.name +
+    //   ' with details: ' + req.body.description);
+
+
+     leaderships.findByIdAndUpdate(req.params.leaderId,{
+
+          $set:req.body
+
+        },
+        {
+          new:true
+        },function  (err,leadership) {
+          
+            if(err) throw err;
+            res.json(leadership);
+        });
+
+
   })
 
   .delete(function(req, res, next){
-    res.end('Deleting leader: ' + req.params.leaderId);
+    // res.end('Deleting leader: ' + req.params.leaderId);
+
+    leaderships.findByIdAndRemove(req.params.leaderId, function (err, resp) {  
+
+          if (err) throw err;
+        
+          res.json(resp);
+    });
+    
   });
 
 module.exports = leaderRouter;
